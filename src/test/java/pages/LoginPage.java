99244b887aed34b97429c2dbe018c6a24fb350c1
erenozpar <eren.ozpar@yahoo.com>
2025-03-04 16:11:03 -0600

    Eren_v3

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,11 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
