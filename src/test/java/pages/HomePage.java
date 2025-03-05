2581641834a8436764f0089cc9c10fd1240ff3b2
erenozpar <eren.ozpar@yahoo.com>
2025-03-05 16:21:48 -0600

    Eren_v7

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -14,11 +14,39 @@ public class HomePage {
 
     }
 
-    //LeftSide MenuBar Category Navigation
+    //Header Navbar Menu
+    @FindBy(xpath = "//button[.='ShopNow']")
+    public static WebElement shopNowButton;
+
+    @FindBy(xpath = "//*[.='English']")
+    public static WebElement languageListBox;
+
+    @FindBy(xpath = "//a[.='Exclusive']")
+    public static WebElement exclusiveButton;
+
+    @FindBy(xpath = "//a[@href='/home']")
+    public static WebElement homeButton;
+
+    @FindBy(xpath = "//a[@href='/contact']")
+    public static WebElement contactButton;
+
+    @FindBy(xpath = "//a[@href='/about']")
+    public static WebElement aboutButton;
 
     @FindBy(xpath = "//a[@href='/sign-up']")
-    public static WebElement SignUpButton;
+    public static WebElement signUpButton;
 
+    @FindBy(xpath = "//input[@class='MuiInputBase-input css-aae3xl']")
+    public static WebElement searchBar;
+
+    @FindBy(xpath = "//*[@data-testid='FavoriteBorderOutlinedIcon']")
+    public static WebElement allFavoriteIcon;
+
+    @FindBy(xpath = "//*[@data-testid='ShoppingCartOutlinedIcon']")
+    public static WebElement shoppingCartIcon;
+
+
+    //LeftSide MenuBar Category Navigation
     @FindAll(@FindBy(xpath = "//li[@class='MuiListItem-root MuiListItem-gutters css-19kxtar']"))
     public List<WebElement> categoryMenu;
 
@@ -49,9 +77,7 @@ public class HomePage {
     @FindBy(xpath = "//span[.='Health & Beauty']")
     public WebElement healthAndBeauty;
 
-
     //Categories-Browse By Category
-
     @FindBy(xpath = "//h6[.='Phones']")
     public WebElement phones;
 
@@ -69,5 +95,39 @@ public class HomePage {
 
     @FindBy(xpath = "//h6[.='Gaming']")
     public WebElement gaming;
+
+    //Product Card
+    @FindAll(@FindBy(xpath = "//button[.='Add To Cart']"))
+    public List<WebElement> addToCartButton;
+
+    @FindAll(@FindBy(xpath = "//*[@data-testid='FavoriteIcon']"))
+    public List<WebElement> favoriteIcon;
+
+    @FindAll(@FindBy(xpath = "//*[@data-testid='VisibilityIcon']"))
+    public List<WebElement> visibilityIcon;
+
+    @FindAll(@FindBy(xpath = "//p[contains(@class,'css-rvr1xv')]"))
+    public List<WebElement> productName;
+
+    @FindAll(@FindBy(xpath = "//p[contains(@class,'css-1rh31si')]"))
+    public List<WebElement> productNewPrice;
+
+    @FindAll(@FindBy(xpath = "//p[contains(@class,'css-ur63k9')]"))
+    public List<WebElement> productOldPrice;
+
+    @FindAll(@FindBy(xpath = "//span[contains(@class,'css-1dp2a72')]"))
+    public List<WebElement> ratingStars;
+
+    @FindAll(@FindBy(xpath = "//p[contains(@class,'css-172okca')]"))
+    public List<WebElement> ratingNumbers;
+
+    //Flash Sales & Our Products
+    @FindAll(@FindBy(xpath = "//button[.='View All Products']"))
+    public List<WebElement> viewAllProductsButton;
+
+    //Best Selling Products
+    @FindBy(xpath = "//button[.='View All Products']")
+    public WebElement viewAllButton;
+
 
 }
